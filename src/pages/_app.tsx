0dec54f6a6d884b147d72a37632241d3652fde30
Alexey Ovsyannikov <soliterco@gmail.com>
2023-03-01 23:22:22 +0300

    changed basic pages files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,41 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import config from '@config/config.json';
+import theme from '@config/theme.json';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import TagManager from 'react-gtm-module';
+import 'styles/style.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const pf = theme.fonts.fontFamily.font;
+  const [fontcss, setFontcss] = useState();
+  useEffect(() => {
+    fetch(`https://fonts.googleapis.com/css2?family=${pf}&display=swap`).then((res) =>
+      res.text().then((css) => setFontcss(css)),
+    );
+  }, [pf]);
+
+  const tagManagerArgs = {
+    gtmId: config.params.tag_manager_id,
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      process.env.NODE_ENV === 'production' && config.params.tag_manager_id && TagManager.initialize(tagManagerArgs);
+    }, 5000);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `${fontcss}`,
+          }}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,80 @@
-import Head from 'next/head'
-// import { Inter } from 'next/font/google'
+// import { Nunito_Sans } from 'next/font/google'
+import config from '@config/config.json';
+import social from '@config/social.json';
+import Base from '@layouts/Baseof';
+import ImageFallback from '@layouts/components/ImageFallback';
+import Pagination from '@layouts/components/Pagination';
+import Post from '@layouts/components/Post';
+import Social from '@layouts/components/Social';
+import { getSinglePage } from '@lib/contentParser';
+import { sortByDate } from '@lib/utils/sortFunctions';
+import { markdownify } from '@lib/utils/textConverter';
+const { blog_folder } = config.settings;
 
-// const inter = Inter({ subsets: ['latin'] })
+// const font = Nunito_Sans({
+//   weight: ['400', '600', '700'],
+//   subsets: ['latin']
+// })
 
-export default function Home() {
+const Home = ({ posts }) => {
+  const { pagination } = config.settings;
+  const { name, image, designation, bio, bio1 } = config.profile;
+  const sortPostByDate = sortByDate(posts);
+
   return (
-    <>
-      <Head>
-        <title>Главная - Ру Малифо</title>
-        <meta name="description" content="Malifaux в России. Новости, переводы рассказов, отчеты с игр." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/M3ed.png" />
-      </Head>
-      <main>
-       
-      </main>
-    </>
-  )
-}
+    <Base>
+      {/* profile */}
+      <div className="section">
+        <div className="container">
+          <div className="row">
+            <div className="mx-auto text-center lg:col-8">
+              <ImageFallback
+                className="mx-auto rounded-full"
+                src={image}
+                width={220}
+                height={220}
+                priority={true}
+                alt={name}
+              />
+              {markdownify(name, 'h1', 'mt-12 text-6xl lg:text-8xl font-semibold')}
+              {markdownify(designation, 'p', 'mt-6 text-primary text-xl')}
+              {markdownify(bio, 'p', 'mt-4 leading-9 text-xl')}
+              {/* {markdownify(bio1, "p", "mt-4 leading-9 text-xl")} */}
+              <Social source={social} className="profile-social-icons mt-8" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* posts */}
+      <div className="pt-4">
+        <div className="container">
+          <div className="row">
+            <div className="mx-auto lg:col-10">
+              <div className="row">
+                {sortPostByDate.slice(0, pagination).map((post, i) => (
+                  <Post className="col-12 mb-6 sm:col-6" key={'key-' + i} post={post} />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-12">
+            <Pagination totalPages={Math.ceil(posts.length / pagination)} currentPage={1} />
+          </div>
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default Home;
+
+// for homepage data
+export const getStaticProps = async () => {
+  const posts = getSinglePage(`content/${blog_folder}`);
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+};
